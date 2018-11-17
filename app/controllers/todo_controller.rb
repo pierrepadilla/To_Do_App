af327476b55fc6fcdb8a8f10e194b103d2f99414
Pierre <pierrepadilla22@gmail.com>
2018-11-17 19:33:06 +0000

    save

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,7 +1,18 @@
 class TodoController < ApplicationController
     def show
-   @todo_description="hello"
-   @todo_pomodoro_estimate=6
+        @todo_description="hello"
+        @todo_pomodoro_estimate=6
+        todo_id=params[:id]
+        if todo_id=='1'
+            @todo_description="shop"
+            @todo_pomodoro_estimate = "60"
+        elsif todo_id=='2'
+            @todo_description="walking"
+            @todo_pomodoro_estimate = "30"
+        elsif todo_id=='3'
+            @todo_description="homework"
+            @todo_pomodoro_estimate = "80"
+        end
     end
     def index
     end
