c836fa822816850327533e7bb5198c79436abdda
Pierre <pierrepadilla22@gmail.com>
2018-11-18 20:12:45 +0000

    heroku

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,5 +1,6 @@
 class TodoController < ApplicationController
     def show
+       @todo=Todo.find_by_id(params[:id])
         @todo_description="hello"
         @todo_pomodoro_estimate=6
         todo_id=params[:id]
@@ -17,6 +18,37 @@ class TodoController < ApplicationController
             @todo_pomodoro_estimate = "45"
         end
     end
+   
     def index
+        @todos = Todo.all
+    end
+   
+    def new
+    end
+    
+    def create
+        t=Todo.new
+        t.description=params['description']
+        t.pomodoro_estimate=params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{t.id}"
+    end
+   
+    def edit
+        @todo = Todo.find_by_id(params[:id])
+    end
+   
+    def update
+     t = Todo.find_by_id(params['id'])
+     t.description = params['description']
+     t.pomodoro_estimate = params['pomodoro_estimate']
+     t.save
+     redirect_to "/todo/show/#{t.id}"
+    end
+    def destroy
+     t = Todo.find_by_id(params[:id])
+     t.destroy
+     redirect_to "/todo/index"
     end
 end
+
